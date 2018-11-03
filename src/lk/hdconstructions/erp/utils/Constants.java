92149077acebf8585b6358859afc30defea96e87
Malith Bandara <malithpb@gmail.com>
2018-11-03 07:53:31 +0530

    Implemented Database connection

--- src/lk/hdconstructions/erp/utils/Constants.java
+++ src/lk/hdconstructions/erp/utils/Constants.java
@@ -1,8 +1,13 @@
 package lk.hdconstructions.erp.utils;
 
+
+/**
+ *
+ * @author Malith Bandara
+ */
 public class Constants {
 	
 	public static final String DB_USERNAME = "root";
 	public static final String DB_PASSWORD = "pass123$";
-
+	public static final String DB_URL = "";
 }
--- src/lk/hdconstructions/erp/utils/DBConnection.java
+++ src/lk/hdconstructions/erp/utils/DBConnection.java
@@ -1,5 +1,48 @@
 package lk.hdconstructions.erp.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author Malith Bandara
+ */
 public class DBConnection {
 
+	private Connection conn;
+	private static DBConnection connectionInstance;
+	
+	private DBConnection() throws SQLException, ClassNotFoundException{
+		conn = createConnection();
+	}
+	
+	public Connection getConnection() {
+		return this.conn;
+	}
+	
+	public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+		
+		if(connectionInstance == null) {
+			connectionInstance = new DBConnection();
+		} else if (connectionInstance.getConnection().isClosed()) {
+			connectionInstance = new DBConnection();
+		}
+		
+		return connectionInstance;
+		
+	}
+	
+	private Connection createConnection() throws SQLException, ClassNotFoundException {
+		
+		String username = Constants.DB_USERNAME;
+		String password = Constants.DB_PASSWORD;
+		String url = Constants.DB_URL;
+		
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		Connection conn = DriverManager.getConnection(url, username, password);
+		return conn;
+		
+	}
 }
